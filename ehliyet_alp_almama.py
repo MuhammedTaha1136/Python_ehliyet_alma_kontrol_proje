ee3be4cd5b3ca48ac3fb81c962b06dcbb758561a
Muhammed Taha Kösem <97670352+MuhammedTaha1136@users.noreply.github.com>
2022-03-09 21:56:49 +0300

    Add files via upload

---
+++ ehliyet_alp_almama.py
@@ -0,0 +1,41 @@
+#Ehliyet alma ve almama tessti.
+
+yas=int(input("Yaşınızı Giriniz :"))
+
+if yas > 18:
+        print("Ehliyet alabilirsiniz.")
+        
+        ehliyetVarmi =str(input("ehliyetiniz varmı, varsa nedir ? :"))
+        print(ehliyetVarmi)
+        
+        if ehliyetVarmi == "B": 
+                print("B sınıfı ehliyeti ile sadece otomobil veya kamyonet kullanabilirsiniz. ")
+    
+        if ehliyetVarmi == "E" :
+                print("E sınıfı ehliyeti ile bir çok araç türünü kullanabilirsiniz. ")
+    
+if yas < 18:
+    print("Ehliyet Alamazsınız")
+
+# İsterseniz sizde benim yaptığım gibi yapıp, daha da ilerletebilirsiniz.
+
+
+
+ 
+ 
+ 
+
+    
+
+
+
+
+ 
+
+
+
+
+
+    
+
+
